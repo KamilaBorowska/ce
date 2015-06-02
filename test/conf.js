0f080af8450b988e9c3c1739de33e2112c046fa2
Konrad Borowski <x.fix@o2.pl>
2015-06-02 14:20:23 +0200

    I give up, use xvfb instead of Sauce Labs.

--- test/conf.js
+++ test/conf.js
@@ -4,15 +4,3 @@ var config = exports.config = {
     framework: 'jasmine2',
     specs: ['spec.js'],
 }
-
-if (env.SAUCE_USERNAME && env.SAUCE_ACCESS_KEY) {
-    config.sauceUser = env.SAUCE_USERNAME
-    config.sauceKey = env.SAUCE_ACCESS_KEY
-}
-if (env.TRAVIS_BUILD_NUMBER) {
-    process.capabilities = {
-        'browserName': 'chrome',
-        'tunnel-identifier': env.TRAVIS_JOB_NUBMER,
-        'build': env.TRAVIS_BUILD_NUMBER
-    }
-}
