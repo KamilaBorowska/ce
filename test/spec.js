bb9014d19322770c150f861e23832e0f481e7209
Konrad Borowski <x.fix@o2.pl>
2015-06-02 12:00:11 +0200

    Add test for participants.

--- test/spec.js
+++ test/spec.js
@@ -6,4 +6,11 @@ describe('C&E Generator', function() {
     it('should have a title', function hasTitle() {
         expect(browser.getTitle()).toEqual('C&E Generator')
     })
+
+    it('should display allowed number of participants', function participants() {
+        element(by.cssContainingText('option', 'NU')).click()
+        element(by.model('ce.participants')).getText().then(function (options) {
+            expect(options.trim().split(/\s+/)).toEqual(['8', '16', '32', '64'])
+        })
+    })
 })
