31cba2ca9a57ae66405274094539af51b678cddb
Konrad Borowski <x.fix@o2.pl>
2015-06-02 13:41:36 +0200

    Actually use tunnel for Sauce.

--- test/conf.js
+++ test/conf.js
@@ -1,10 +1,18 @@
-exports.config = {
+var env = process.env
+
+var config = exports.config = {
     framework: 'jasmine2',
     specs: ['spec.js'],
 }
 
-if (process.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY) {
-    exports.config.sauceSeleniumAddress = 'localhost:4445/wd/hub'
-    exports.config.sauceUser = process.env.SAUCE_USERNAME
-    exports.config.sauceKey = process.env.SAUCE_ACCESS_KEY
+if (env.SAUCE_USERNAME && env.SAUCE_ACCESS_KEY) {
+    config.sauceSeleniumAddress = 'localhost:4445/wd/hub'
+    config.sauceUser = env.SAUCE_USERNAME
+    config.sauceKey = env.SAUCE_ACCESS_KEY
+}
+if (env.TRAVIS_BUILD_NUMBER) {
+    process.capabilities = {
+        'tunnel-identifier': env.TRAVIS_BUILD_NUBMER,
+        'build': env.TRAVIS_BUILD_NUMBER
+    }
 }
